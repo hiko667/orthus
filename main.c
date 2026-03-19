6965f41d9771ed0105edb34e1a36cb019fff6d82
marty <123@123.pl>
2026-03-19 20:02:29 +0100

    add: basic directory validation to main.c

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
+int is_dir(const char *path) {
+	struct stat st;
+	return (stat(path, &st) == 0) && S_ISDIR(st.st_mode);
+}
 
 int main(int argc, char * argv[])
 {
-    printf("Niggga");
+	if (argc < 3) {
+		printf("Please provide two paths to directories you wish to synchronize. \n");
+		return 1;
+	}
+
+	if (!is_dir(argv[1]) || !is_dir(argv[2])) {
+		printf("Please provide valid paths to directories. \n");
+		return 1;
+	}
+
+
     return 0;
 }
