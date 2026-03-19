6f83c3044b3fa2a4bf13c09d37909bc603716a62
hiko667 <91916@student.pb.edu.pl>
2026-03-19 22:18:18 +0100

    added config struct. Wrote some ideas in comments

---
+++ config_struct.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_STRUCT_H
+#define CONFIG_STRUCT_H
+#include <stdbool.h>
+struct configStruct
+{
+    char sourceDir[128];
+    char targetDir[128];
+    int awakeningFrequency; 
+    bool recursive;
+    int minSizeToBeBig;
+};
+
+
+#endif
--- main.c
+++ main.c
@@ -11,6 +11,20 @@ void daemonize() {
 
 }
 
+/*
+Potrzebujemy następujących funkcjonalności określanych argumentami funckcji main()
+- target directory
+- src drectory
+- częstotliwośc wybudzania
+- opcje --help; bo tak i to cool; opcjonalnie flagę -h
+- ten -R, aka żeby robić rekursywnie
+- argument do granicy rozdzielającej pliki
+proponowana struktura wywołania
+orthus -s ./source -t ./target -f 5 -R -m 5000
+gdzie po -s następuje źródło, po -t cel, po -f co ile minut wybudzać
+po -R nic, po -m ile bajtów to granica
+
+*/
 int main(int argc, char * argv[])
 {
 	if (argc < 3) {
